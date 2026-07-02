f6f3ec2dd4a50895a2565b3934a78e649b82da55
dilmun1101 <dilmun94@gmail.com>
2026-07-02 21:17:13 +0500

    feat: add auth-provider, get-current-user

---
+++ src/shared/api/supabase/get-current-user.ts
@@ -0,0 +1,15 @@
+import 'server-only';
+import { createClient } from './server';
+import type { User } from '@supabase/supabase-js';
+
+export async function getCurrentUser(): Promise<User | null> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getClaims();
+
+  if (error || !data?.claims) {
+    return null;
+  }
+
+  return data.claims as unknown as User;
+}
