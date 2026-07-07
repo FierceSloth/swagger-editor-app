9a86626b6ff6b4421278b8a194d72d8b62024a7f
Anna <dilmun1101@gmail.com>
2026-07-07 20:01:28 +0500

    Story/RSS-CORE-00_fixAuth (#24)
    * chore: change name of env
    
    * refactor: add edits
    
    * chore: change package-lock.json
    
    * chore: change package-lock
    
    * chore: change package-lock
    
    * fix: fix package-lock
    
    ---------
    
    Co-authored-by: dilmun1101 <dilmun94@gmail.com>

--- src/features/auth/model/action.ts
+++ src/features/auth/model/action.ts
@@ -3,6 +3,7 @@
 import { redirect } from 'next/navigation';
 import { createClient } from '@/shared/api/supabase/server';
 import getFormValue from '@/shared/api/utils/form';
+import { ROUTES } from '@/shared/config/routes';
 
 export type AuthError = 'invalidCredentials' | 'userAlreadyExists' | 'tooManyRequests' | 'unknownError';
 
@@ -41,7 +42,7 @@ export async function signInWithPassword(formData: FormData): Promise<AuthAction
     return { error: mapAuthError(error.code, error.message) };
   }
 
-  redirect('/');
+  redirect(ROUTES.HOME);
 }
 
 export async function signUp(formData: FormData) {
@@ -59,7 +60,7 @@ export async function signUp(formData: FormData) {
     return { error: mapAuthError(error.code, error.message) };
   }
 
-  redirect('/');
+  redirect(ROUTES.HOME);
 }
 
 export async function signOut() {
@@ -67,5 +68,5 @@ export async function signOut() {
 
   await supabase.auth.signOut();
 
-  redirect('/login');
+  redirect(ROUTES.LOGIN);
 }
