65bce6345fd65f2dcfc4f36cfb4d4dc192ff69ea
dilmun1101 <dilmun94@gmail.com>
2026-07-12 22:26:59 +0500

    feat: add logo

--- src/pages/about-page/ui/about-hero-section/about-hero-section.tsx
+++ src/pages/about-page/ui/about-hero-section/about-hero-section.tsx
@@ -3,6 +3,7 @@ import { getTranslations } from 'next-intl/server';
 import styles from './about-hero-section.module.scss';
 import { GlassCard } from '@/shared/ui/glass-card';
 import { ArrowIcon } from '@/shared/ui/arrow-icon';
+import Image from 'next/image';
 
 const RS_SCHOOL_LINK = 'https://rs.school/courses/reactjs';
 
@@ -12,7 +13,9 @@ export async function AboutHeroSection() {
   return (
     <div className={styles.container}>
       <div className={styles.containerLabel}>
-        <GlassCard className={styles.courseLabel}>RS</GlassCard>
+        <GlassCard className={styles.courseLabel}>
+          <Image className={styles.logo} src="/icons/rs.png" alt="RS School" width={210} height={210} />
+        </GlassCard>
       </div>
       <div className={styles.content}>
         <p className={styles.label}>{t('label')}</p>
