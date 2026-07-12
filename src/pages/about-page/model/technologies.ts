5d6b59c9b3488dad2fb7ad6f8171baf909fa35b0
dilmun1101 <dilmun94@gmail.com>
2026-07-12 14:58:50 +0500

    feat: add information

---
+++ src/pages/about-page/model/technologies.ts
@@ -0,0 +1,45 @@
+import reactIcon from '../assets/icons/react.png';
+import typescriptIcon from '../assets/icons/typescript.png';
+import nextjsIcon from '../assets/icons/nextjs.png';
+import scssIcon from '../assets/icons/sass.png';
+import viteIcon from '../assets/icons/vite.png';
+import supabaseIcon from '../assets/icons/supabase.png';
+
+export interface TechnologyItem {
+  id: 'react' | 'typescript' | 'nextjs' | 'sass' | 'vite' | 'supabase';
+  name: string;
+  icon: typeof reactIcon;
+}
+
+export const technologiesConfig: TechnologyItem[] = [
+  {
+    id: 'react',
+    name: 'React',
+    icon: reactIcon,
+  },
+  {
+    id: 'typescript',
+    name: 'TypeScript',
+    icon: typescriptIcon,
+  },
+  {
+    id: 'nextjs',
+    name: 'Next.js',
+    icon: nextjsIcon,
+  },
+  {
+    id: 'sass',
+    name: 'SASS',
+    icon: scssIcon,
+  },
+  {
+    id: 'vite',
+    name: 'Vite',
+    icon: viteIcon,
+  },
+  {
+    id: 'supabase',
+    name: 'Supabase',
+    icon: supabaseIcon,
+  },
+];
