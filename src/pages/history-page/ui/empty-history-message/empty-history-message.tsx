f7eb11eb7ea140e836cbb4e59370f3bb0e5dc76f
dilmun1101 <dilmun94@gmail.com>
2026-07-13 18:51:35 +0500

    feat: add styles for history

--- src/pages/history-page/ui/empty-history-message/empty-history-message.tsx
+++ src/pages/history-page/ui/empty-history-message/empty-history-message.tsx
@@ -1,12 +1,13 @@
 import { getTranslations } from 'next-intl/server';
+import styles from './empty-history-message.module.scss';
 
 export async function EmptyHistoryMessage() {
   const t = await getTranslations('History');
 
   return (
-    <div>
-      <h2>{t('emptyTitle')}</h2>
-      <p>{t('emptyDescription')}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{t('emptyTitle')}</h1>
+      <p className={styles.description}>{t('emptyDescription')}</p>
     </div>
   );
 }
--- src/pages/history-page/ui/history-page.tsx
+++ src/pages/history-page/ui/history-page.tsx
@@ -6,7 +6,6 @@ import { getTranslations } from 'next-intl/server';
 import { HistoryIntro } from './history-intro/history-intro';
 import { HistoryRequestCard } from './history-requst-card/history-request-card';
 import styles from './history-page.module.scss';
-
 interface HistoryPageProps {
   items: HistoryItem[];
 }
